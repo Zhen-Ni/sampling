d7a089e5d3e8d3784253413a36cb0be2a16bfb02
Zhen-Ni <z.ni@hotmail.com>
2022-09-28 20:25:23 +0800

    add gaussian distribution generator using Box-Muller method

--- sampling.py
+++ sampling.py
@@ -2,6 +2,7 @@
 
 """Random variables subject to user-defined distributions."""
 
+import math
 import random
 
 from typing import Callable
@@ -12,6 +13,23 @@ mpl_fontpath = mpl.get_data_path() + '/fonts/ttf/STIXGeneral.ttf'
 mpl_fontprop = mpl.font_manager.FontProperties(fname=mpl_fontpath)
 plt.rc('font', family='STIXGeneral', weight='normal', size=10)
 plt.rc('mathtext', fontset='stix')
+
+
+def gaussian() -> float:
+    """Samples from standard Gaussian distribution.
+
+    The Box-Muller method is used for generating the
+    random variables. The Box-Muller method can
+    generate two gaussian random variables at a time.
+    But in this function, only one random variable
+    will be calculated and returned.
+    """
+    r2 = 2.
+    while 1 < r2:
+        z1 = random.random() * 2 - 1
+        z2 = random.random() * 2 - 1
+        r2 = z1 ** 2 + z2 ** 2
+    return z1 * (-math.log(r2) / r2) ** .5
 
 
 def rejection(pdf: Callable[[float], float],
@@ -48,12 +66,20 @@ def rejection(pdf: Callable[[float], float],
     return res
 
 
+def _test_gaussian():
+    samples = [gaussian() for i in range(100000)]
+    plt.figure()
+    plt.hist(samples, bins=50, density=True)
+
+
 def _test_rejection():
     pdf = lambda x: 1 - x ** 2
     samples = [rejection(pdf, -1, 1, 1) for i in range(100000)]
+    plt.figure()
     plt.hist(samples, bins=50, density=True)
 
 
 if __name__ == '__main__':
+    _test_gaussian()
     _test_rejection()
     plt.show()
